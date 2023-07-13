57b8c9a07285732f4ff37b493abf5ac2181b7973
MALIK DINAR <malikdinaras@gmail.com>
2023-07-13 18:36:59 +0530

    test

--- learn.js
+++ learn.js
@@ -1,15 +1,29 @@
 
-let a=20;
+// let a=20;
 
 
-const b=()=>{
-    console.log(a)
-    const c=()=>{
-        console.log(a)
-        let crucial="hello"
-        console.log(crucial);
-    }
-    c();
+// const b=()=>{
+//     console.log(a)
+//     const c=()=>{
+//         console.log(a)
+//         let crucial="hello"
+//         console.log(crucial);
+//     }
+//     c();
+// }
+
+// b()
+
+let i=0
+while(6<5){
+    console.log(i)
+    i++
 }
 
-b()
+console.log('dowhile');
+
+i=0;
+do{
+    console.log(i)
+    i++
+}while(6<5)
