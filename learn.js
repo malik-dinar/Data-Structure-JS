5068f6e5b58b63cbd757f6099c10388ecb341a7e
MALIK DINAR <malikdinaras@gmail.com>
2023-07-14 12:27:25 +0530

    learn

--- learn.js
+++ learn.js
@@ -14,16 +14,30 @@
 
 // b()
 
-let i=0
-while(6<5){
-    console.log(i)
-    i++
-}
 
-console.log('dowhile');
+// while do while
 
-i=0;
-do{
-    console.log(i)
-    i++
-}while(6<5)
+// let i=0
+// while(6<5){
+//     console.log(i)
+//     i++
+// }
+
+// console.log('dowhile');
+
+// i=0;
+// do{
+//     console.log(i)
+//     i++
+// }while(6<5)
+
+
+// reduce
+
+let array = [1,2,30,4,5,6,7,8,9,10]
+
+const sum1 = array.reduce((acc,curr)=>{
+    return acc + curr
+})
+
+console.log(sum1);
