ea5ffb23a8a53dfc37eb62eb9cf3846de1f0ce19
MALIK DINAR <malikdinaras@gmail.com>
2023-07-28 23:55:49 +0530

    tricky js

---
+++ tricky js/one.js
@@ -0,0 +1,11 @@
+let arrr = [{value:'hi'},'hi',0,1];
+
+delete arrr[3]
+
+console.log(arrr);
+
+console.log(arrr.length);
+
+obj :{
+    console.log(hello);
+}
---
+++ tricky js/two.js
@@ -0,0 +1,8 @@
+function getY(){
+    y = 7;
+    console.log(y);
+}
+
+let y = 10;
+getY()
+console.log(y);
